f94d41b375d08075d3e3cbad040d5cf76b914e4f
Eilidh Hendry <ehendry91@gmail.com>
2016-01-28 14:13:41 +1100

    Adds pagination to blog
    Shortens blog posts to preview of 200 words on posts page
    Splits blog into multiple pages
    Puts post into BS panels
    Adds buttons to scroll through all pages and go to prev or next page
    Adds share options

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render_to_response, render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blogengine.models import Post
 
 def getRecentPosts(request):
     posts = Post.objects.all()
-
     sorted_posts = posts.order_by('-published_date')
-
-    return render_to_response('blog/posts.html', {'posts':sorted_posts})
+    paginator = Paginator(sorted_posts, 2) 
+    
+    page = request.GET.get('page')
+    try:
+        post_list = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an int give page 1
+        post_list = paginator.page(1)     
+    except EmptyRange:
+        # if page out of range give last page
+        post_list = paginator.page(paginator.num_pages) 
+    
+    return render(request, 'blog/posts.html', {'posts':post_list})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
