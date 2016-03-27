b95facaea0773dcb4be35deafffb73c6e4137127
Eilidh Hendry <ehendry91@gmail.com>
2016-03-27 20:48:21 +1100

    adds wysiwyg editor to blog admin

--- blogengine/admin.py
+++ blogengine/admin.py
@@ -1,4 +1,17 @@
 import models
+from redactor.widgets import RedactorEditor
 from django.contrib import admin
+from django import forms
 
-admin.site.register(models.Post)
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        model = models.Post
+        widgets = {
+                'text': RedactorEditor(),
+                }
+        fields = "__all__"
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+admin.site.register(models.Post, PostAdmin)
